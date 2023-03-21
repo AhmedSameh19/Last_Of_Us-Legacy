4d3387ea0423f7bc81959574c6332a4f2870ad89
Home <Home@DESKTOP-8DDNLN2>
2023-03-21 18:14:00 +0200

    error in cell classs

--- CS4/src/model/world/Cell.java
+++ CS4/src/model/world/Cell.java
@@ -3,11 +3,5 @@ package model.world;
 public abstract class Cell {
 	private boolean isVisible;
 
-	public boolean isVisible() {
-		return isVisible;
-	}
 
-	public void setVisible(boolean isVisible) {
-		this.isVisible = isVisible;
-	}
 }
